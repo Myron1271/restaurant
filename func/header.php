3fafdbbfd587ccd45c44c814e677cb92e1fe00ac
Myron <myronmax_seelen@live.nl>
2019-05-17 21:22:09 +0200

    Header Commit

--- func/header.php
+++ func/header.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Myron
- * Date: 17 May 2019
- * Time: 21:16
- */
+
+<link rel="stylesheet" as="style" type="text/css" href="/css/style.css">
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" id="HomeText" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto" id="NavBarText">
+            <li class="nav-item">
+                <a class="nav-link" id="NavBarTextSpelersToevoegen" href="./SpelersToevoegen.php">Reseveren</a>
+            </li>
+            <!--
+            <li class="nav-item">
+                <a class="nav-link" id="NavBarTextSpelersToevoegen" href="./SpelersOnderhoud.php">Spelers Onderhouden</a>
+            </li>
+            -->
+    </div>
+</nav>
+</header>
--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 <!doctype html>
 <html lang="en">
 <head>
+
+    <link rel="stylesheet" as="style" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -16,6 +21,12 @@
 </head>
 
 <body background="img/Restaurant-Backround1.png" style="background-size: cover";>
+
+<header>
+    <?php
+        include_once 'func/header.php';
+    ?>
+</header>
 
 
 
