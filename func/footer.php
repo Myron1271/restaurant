96c4b4793e4ecc17be4c62f73f2b7a86218571ad
Myron <myronmax_seelen@live.nl>
2019-05-17 21:32:03 +0200

    style Commit 2

--- func/footer.php
+++ func/footer.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Myron
- * Date: 17 May 2019
- * Time: 21:16
- */
+<link rel="stylesheet" as="style" type="text/css" href="/css/style.css">
+
+
+<footer class="page-footer font-small fixed-bottom position-fixed" id="FooterColor" style="width: 100%">
+    <div class="container">
+        <div class="row text-center d-flex justify-content-center pt-4 mb-3 FooterText">
+            <div class="col-md-2 mb-3">
+                <a href="./Geschiedenis.php">Geschiedenis</a>
+            </div>
+            <div class="col-md-2 mb-3">
+                <a href="./Fotos.php">Foto's</a>
+            </div>
+            <div class="col-md-2 mb-3">
+                <a href="./Contact.php">Contact</a>
+            </div>
+        </div>
+        <hr class="FooterLine " style="margin: 0 20%;">
+        <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+            <div class="col-md-8 pt-3 text-dark">
+                <p>Vind hier alle informatie over de Leeuwenborghse Voetbal Vereniging!</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-copyright text-center py-3 bg-dark text-white">
+        © 2019 Copyright
+    </div>
+
+
+</footer>
--- func/header.php
+++ func/header.php
@@ -2,22 +2,62 @@
 <link rel="stylesheet" as="style" type="text/css" href="/css/style.css">
 
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" id="HomeText" href="index.php">Home</a>
-    <button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto" id="NavBarText">
-            <li class="nav-item">
-                <a class="nav-link" id="NavBarTextSpelersToevoegen" href="./SpelersToevoegen.php">Reseveren</a>
-            </li>
-            <!--
-            <li class="nav-item">
-                <a class="nav-link" id="NavBarTextSpelersToevoegen" href="./SpelersOnderhoud.php">Spelers Onderhouden</a>
-            </li>
-            -->
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" id="HomeText" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto" id="NavBarText">
+                <li class="nav-item">
+                    <a class="nav-link" id="NavBarTextGeschiedenis" href="./Geschiedenis.php">Geschiedenis</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="NavBarTextFotos" href="./Fotos.php">Foto's</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="NavBarTextContact" href="./Contact.php">Contact</a>
+                </li>
+                <!--<ul class="navbar-nav ml-auto" id="NavBarText">
+                    <li class="nav-item">
+                        <a class="nav-link" id="NavBarTextRegisteren" href="./Account.php">Account</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" id="NavBarTextRegisteren" href="./Inloggen.php">Inloggen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="NavBarTextInloggen" href="./Registreren.php">Registreren</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="NavBarTextInloggen" href="./Uitloggen.php">Uitloggen</a>
+                    </li>
+                </ul>
+                -->
+            </ul>
+            <ul class="nav navbar-nav navbar-right" id="NavBarText">
+                <li class="nav-item">
+                    <a class="nav-link" id="Username" href="./Account.php"></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="NavBarTextAccount" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Account
+                    </a>
+                    <div style="padding-right: 150px">
+                        <div style="text-align: center" class="dropdown-menu animate rotateInDownRight" aria-labelledby="NavBarTextAccount">
+                            <a class="dropdown-item" id="NavBarTextDropDown" href="./Inloggen.php">Inloggen</a>
+                            <a class="dropdown-item" id="NavBarTextDropDown" href="./Registreren.php">Registreren</a>
+                            <hr class="FooterLine " style="margin: 0 20%;">
+                            <a class="dropdown-item" id="NavBarTextDropDown" href="./Uitloggen.php">Uitloggen</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <script type="text/javascript">
+        var Username = '<?php echo $_SESSION['user'];?>';
+        document.getElementById('Username').innerHTML= Username;
+    </script>
 </header>
